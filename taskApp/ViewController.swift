d319fe26d9f7e189262b284b32bb9d08e8f229ed
yhara <general@y-hara.com>
2025-01-15 23:09:59 +0900

    Initial Commit

---
+++ taskApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  taskApp
+//
+//  Created by y.hara on 2025/01/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
